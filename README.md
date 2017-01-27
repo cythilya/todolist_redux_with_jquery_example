9d57e6400f9051dd82cfb44233e43e1297f204e4
Cythilya Tang <cythilya@gmail.com>
2017-01-27 15:25:04 +0800

    upload files

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# todolist_redux_with_jquery_example
-Todo List: Redux with jQuery Example
+# Todo List: Redux with jQuery Example
+Redux是用來管理狀態(state)框架，可以和React、Angular、jQuery甚至Pure JavaScript一起使用。
+以下就簡單使用jQuery實作Todo List。
+
+## 功能介紹/Demo
+先來看看完成的樣子。
+
+![](demo/demo_1.gif)
+
+功能有：
+
+- 新增一個task
+- 修改task名稱
+- 刪除一個task
+- 更改task狀態為完成/未完成
+- 切換filter，選擇呈現全部或已完成的task
+
+## 實作
+程式碼主要分成三個部份：
+
+- Reducer：更新state的函式，嚴格限制相同輸入要能得到相同輸出(functional programming的概念)
+- Store：儲存state的地方，用來連接Action和Reducer
+- Action：通知有事情發生，必須更新state
+
+由以上可知，這個Todo List裡面存的資料就是狀態，只要狀態更新，例如新增task、標示一個task為完成，就重新繪製Todo List區塊。而「新增task、標示一個task為完成」都是動作(Action)，動作會告知狀態更新，狀態一改變，Store就會做指定的事情，例如重繪元件。
+
+### Reducer
+更新state的函式，輸入值是目前的狀態和動作，輸出值是下一個狀態。
+
+這裡宣告了一個reducer「todos」，初始值是一個物件，包含todos(todo list的內容)、filter(目前呈現的內容，可以是全部all或已完成completed)、counter(指示下一個新增task的ID)。
+
+每當Action通知狀態改變時，除了要代入Action Type外，還要代入需要的資料。
+Action Type可能是「ADD_TODO」等，需要的資料像是task名稱(`actions.name`)，如下範例所示。
+
+    const todos = (state = { todos: [], filter: 'all', counter: 0 }, actions) => { //輸入值：目前的狀態和動作
+      switch(actions.type) {
+        case 'ADD_TODO':
+          state.todos.push({
+            id: state.counter++,
+            name: actions.name,
+            completed: false
+          });
+          break;
+
+        //以下省略...
+      }
+
+      return state; //輸出值：下一個狀態
+    }
+
+### Action
+用來通知有事情發生，必須更新state。
+
+如下所示，當我們按下新增按鈕來增加一個task時，就會到input抓取使用者輸入的文字來更新state，把新的資料加到state裡面－－使用`store.dispatch`來更新狀態，其中要傳入動作的類別「ADD_TODO」和剛抓取的使用者的輸入內容「newValue」。
+
+    $('.add').click(() => {
+      var newValue = $('.input_new_item').val();
+
+      if(newValue) {
+        store.dispatch({
+          type: 'ADD_TODO',
+          name: newValue
+        });
+      }
+    });
+
+### Store
+儲存state的地方，用來連接Action和Reducer。
+
+有以下幾種方法：
+
+- getState()：取得目前狀態
+- dispatch(action)：更新狀態，例如剛剛的例子，新增一個task
+- subscribe(listener)：註冊監聽器
+
+當我們想知道目前的資料/狀態時，就可以用`getState()`取得目前狀態。
+
+  	var state = todosStore.getState(); //取得目前Todo List的內容
+
+更新狀態就使用`dispatch(action)`，同上Action增加一個task的範例。
+
+
+使用`subscribe(listener)`監聽狀態，狀態改變就更新UI。
+
+	  todosStore.subscribe(() => {
+	    //監聽狀態，狀態改變就更新UI
+	  });
+
+
+以上的範例原始碼在[這裡](https://github.com/cythilya/todolist_redux_with_jquery_example)，有興趣的話可以參考看看。
+
+---
+## 參考資料
+- [Redux with jQuery example](https://codepen.io/mdd/full/wGRqbw/)
+- [Read Me · Redux](http://redux.js.org/)
+- [Redux 中文文檔](http://cn.redux.js.org/index.html)
---
+++ script.todolist.js
@@ -0,0 +1,138 @@
+function findPosition(id, array) {
+  return array.map(function(element) {
+    return element.id
+  }).indexOf(id);
+}
+
+//Reducer：更新state的函式
+const todos = (state = { todos: [], filter: 'all', counter: 0 }, actions) => {
+  switch(actions.type) {
+    case 'ADD_TODO':
+      state.todos.push({
+        id: state.counter++,
+        name: actions.name,
+        completed: false
+      });
+      break;
+    case 'DELETE_TODO':
+      state = Object.assign({}, state, state.todos.splice(findPosition(actions.id, state.todos), 1));
+      break;
+    case 'EDIT_TODO':
+      state.todos[findPosition(actions.id, state.todos)].name = actions.name;
+      break;
+    case 'COMPLETE_TODO':
+      state.todos[findPosition(actions.id, state.todos)].completed = actions.completed;
+      break;
+    case 'INCOMPLETE_TODO':
+      state.todos[findPosition(actions.id, state.todos)].completed = actions.completed;
+      break;
+    case 'FILTER_COMPLETED':
+      state.filter = 'completed';
+      break;
+    case 'FILTER_ALL':
+     state.filter = 'all'
+      break;
+    default:
+      break;
+  }
+
+  return state;
+}
+
+//Store：儲存state的地方
+const todosStore = Redux.createStore(todos);
+
+//Action：使用todosStore.dispatch來通知有事情發生，必須更新state
+$('.add').click(() => {
+  var newValue = $('.input_new_item').val();
+  if(newValue) {
+    todosStore.dispatch({
+      type: 'ADD_TODO',
+      name: newValue
+    });
+  }
+
+  $('.input_new_item').val('');
+});
+
+$('.list').on('click', '.delete', function() {
+  todosStore.dispatch({
+    type: 'DELETE_TODO',
+    id: $(this).parents('.item').data('id')
+  });
+});
+
+$('.list').on('click', '.edit', function() {
+  $(this).hide();
+  $(this).parents('.item').find('.edit-block').show();
+});
+
+$('.list').on('click', '.submit', function() {
+  var $item = $(this).parents('.item');
+
+  todosStore.dispatch({
+    type: 'EDIT_TODO',
+    id: $item.data('id'),
+    name: $item.find('.input-edit').val()
+  });
+
+  $item.find('.edit').show();
+  $item.find('.edit-block').hide();
+});
+
+$('.list').on('change', '.complete-status', function() {
+  var isCompleted = $(this).prop('checked');
+  var actionType = 'INCOMPLETE_TODO';
+
+  if(isCompleted) {
+    actionType = 'COMPLETE_TODO'
+  }
+
+  todosStore.dispatch({
+    type: actionType,
+    id: $(this).parents('.item').data('id'),
+    completed: isCompleted
+  });
+
+});
+
+$('.filter-complete').click(() => {
+  todosStore.dispatch({
+    type: 'FILTER_COMPLETED'
+  });
+});
+
+$('.filter-all').click(() => {
+  todosStore.dispatch({
+    type: 'FILTER_ALL'
+  });
+});
+
+//監聽，當dispatch action執行造成state改變時就去取目前state內容，用來更新UI
+todosStore.subscribe(() => {
+  var state = todosStore.getState();
+  var ResultHTML = '';
+
+  for(var i = 0; i < state.todos.length; i++) {
+    let HTML = '';
+    let item = state.todos[i];
+    let status = item.completed ? 'checked' : '';
+    let hide = (state.filter === 'completed' && !item.completed) ? 'hide' : '';
+
+    HTML += [
+      '<li class="item ' + hide + '" data-id="' + item.id + '">',
+      '  <input type="checkbox" class="complete-status"' + status +'>',
+      '  <span class="name">' + item.name + '</span>',
+      '  <span class="edit-block hide">',
+      '    <input class="input-edit" />',
+      '    <button class="submit">submit</button>',
+      '  </span>',
+      '  <button class="edit">edit</button>',
+      '  <button class="delete">delete</button>',
+      '</li>'
+    ].join('');
+    ResultHTML += HTML;
+  }
+
+  $('#todoList .list').html(ResultHTML);
+});
